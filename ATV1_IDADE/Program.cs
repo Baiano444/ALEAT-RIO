c266020473173c1a93c3ae3845fa20e9f6e5e8b3
pedrope2206 <90793651+pedrope2206@users.noreply.github.com>
2022-10-20 08:48:29 -0300

    Update Program.cs
    #hacktoberfest2022

--- ATV1_IDADE/Program.cs
+++ ATV1_IDADE/Program.cs
@@ -3,9 +3,11 @@
 int iddmin = 20;
 int iddmax = 50;
 int iddmedia = 0;
+int idadeavançada = 00000;
 
 Console.WriteLine($"IDADE MINIMA: {iddmin}");
 Console.WriteLine($"IDADE MÁXIMA: {iddmax}");
+Console.WriteLine($"Fudendo com este código")
 
 iddmedia = iddmin + iddmax/2;
 Console.WriteLine("");
